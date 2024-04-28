da6d6e17145b2ccc899868d1941aac00bbc27af7
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-04-29 00:45:55 +0300

    fix footer social icons position

--- src/components/footer/FooterSocialIcons.component.tsx
+++ src/components/footer/FooterSocialIcons.component.tsx
@@ -6,7 +6,7 @@ import styles from './footer-social-icons.module.css';
 export const FooterSocialIconsComponent = () => (
     <div className={styles.footerSocialIcons}>
         <SocialIconComponent iconName={SocialIconName.FACEBOOK} url="https://facebook.com" title="Facebook logo" />
-        <SocialIconComponent iconName={SocialIconName.INSTAGRAM} url="https://instagram.com" title="Instagram logo" />
         <SocialIconComponent iconName={SocialIconName.LINKEDIN} url="https://linkedin.com" title="Linkedin logo" />
+        <SocialIconComponent iconName={SocialIconName.INSTAGRAM} url="https://instagram.com" title="Instagram logo" />
     </div>
 );
