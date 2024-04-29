3317b6ac8d665da540dc878368024c1c217c4789
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-04-29 15:26:13 +0300

    update about me grid

--- src/components/about/AboutMe.component.tsx
+++ src/components/about/AboutMe.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { AboutMeImageComponent } from './AboutMeImage.component.tsx';
-import { AboutMeTextComponent } from './AboutMeText.component.tsx';
+import image from '@/assets/img/aboutme.png';
 
 import styles from './about-me.module.css';
 
@@ -12,13 +11,36 @@ interface AboutMeProps {
 
 export const AboutMeComponent: React.FC<AboutMeProps> = ({ fullName, nikName }) => (
     <div className={styles.aboutMe}>
-        <div className={styles.aboutMeColumn}>
-            <AboutMeTextComponent fullName={fullName} nikName={nikName} />
+        <h1 className={styles.aboutMeHeadline}>About me</h1>
+        <div className={styles.aboutMeTextContainer}>
+            <p className={styles.aboutMeParagraph}>
+                Hi! My name is {fullName} and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
+                React, HTML, CSS, JavaScript and Git version control system.
+            </p>
+            <p className={styles.aboutMeParagraph}>
+                This page was developed during the course{' '}
+                <a
+                    className={styles.aboutMeLink}
+                    href="https://www.mastersacademy.education/frontend-for-beginners-it"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    &apos;Intro to React&apos;
+                </a>{' '}
+                from Masters Academy in 2024.
+            </p>
+            <p className={styles.aboutMeParagraph}>
+                This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own
+                small project for the portfolio.
+            </p>
+            <p className={styles.aboutMeParagraph}>
+                You can check out my{' '}
+                <a className={styles.aboutMeLink} href={`https://github.com/${nikName}`} target="_blank" rel="noreferrer">
+                    GitHub
+                </a>
+                .
+            </p>
         </div>
-        <div className={styles.aboutMeColumn}>
-            <div className={styles.aboutMeImgContainer}>
-                <AboutMeImageComponent />
-            </div>
-        </div>
+        <img className={styles.aboutMeImage} src={image} alt="About me" />
     </div>
 );
--- src/components/about/AboutMeImage.component.tsx
+++
@@ -1,3 +0,0 @@
-import image from '@/assets/img/aboutme.png';
-
-export const AboutMeImageComponent = () => <img src={image} alt="About me" />;
