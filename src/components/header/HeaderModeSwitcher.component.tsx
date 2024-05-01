6604bc429fe0fa61434fb9dad27ba352d418bfd7
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-01 21:19:02 +0300

    detect color scheme in HeaderModeSwitcherComponent

--- src/components/header/HeaderModeSwitcher.component.tsx
+++ src/components/header/HeaderModeSwitcher.component.tsx
@@ -4,14 +4,14 @@ import { ColorModeSunIconComponent } from '../icon/ColorModeSunIcon.component.ts
 
 import styles from './header-mode-switcher.module.css';
 
-export const HeaderModeSwitcherComponent = () => (
-    <div className={styles.headerModeSwitcher}>
-        <a href="/">
-            <ColorModeSunIconComponent />
-        </a>
-        <ColorModeDividerIconComponent />
-        <a href="/">
-            <ColorModeMoonIconComponent />
-        </a>
-    </div>
-);
+export const HeaderModeSwitcherComponent = () => {
+    const shouldShowDarkSchema = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    return (
+        <div className={styles.headerModeSwitcher}>
+            <ColorModeSunIconComponent className={shouldShowDarkSchema ? styles.headerModeInactiveIcon : ''} />
+            <ColorModeDividerIconComponent />
+            <ColorModeMoonIconComponent className={shouldShowDarkSchema ? '' : styles.headerModeInactiveIcon} />
+        </div>
+    );
+};
