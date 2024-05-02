8d9fb606a911d760eaedd221e1d1fb59dbcb48c7
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-02 16:29:16 +0300

    update ProductProps & CategoryProps interfaces

--- src/interfaces/ProductProps.interface.tsx
+++ src/interfaces/ProductProps.interface.tsx
@@ -1,14 +1,12 @@
 import type { CategoryProps } from './CategoryProps.interface.tsx';
 
-type ExcludedCategoryProps = Exclude<CategoryProps, 'image' | 'creationAt' | 'updatedAt'>;
-
 export interface ProductProps {
     id: number;
     title: string;
     price: number;
     description: string;
     images: string[];
-    creationAt: string;
-    updatedAt: string;
-    category: ExcludedCategoryProps;
+    creationAt?: string;
+    updatedAt?: string;
+    category: CategoryProps;
 }
