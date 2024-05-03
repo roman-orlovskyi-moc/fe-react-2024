f7c767895c9f33fea3d6faae403bee96242da0c0
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-03 21:06:10 +0300

    use CartIconCounterComponent in ProductCardComponent

--- src/components/products-list/ProductCard.component.tsx
+++ src/components/products-list/ProductCard.component.tsx
@@ -3,12 +3,15 @@ import React, { useContext } from 'react';
 import { CartContext } from '@/context/CartContext.context.tsx';
 import type { ProductProps } from '@/interfaces/ProductProps.interface.tsx';
 
+import { CartIconCounterComponent } from '../common/CartIconCounter.component.tsx';
 import { CartIconComponent } from '../icon/CartIcon.component.tsx';
 
 import styles from './product-card.module.css';
 
 export const ProductCardComponent: React.FC<ProductProps> = (productData) => {
     const cartContext = useContext(CartContext);
+    const productCartItem = cartContext.cart.items.find((item) => item.id === productData.id);
+    const productCartItemCount = productCartItem ? productCartItem.quantity : 0;
 
     const addToCart = () => {
         cartContext.addToCart({
@@ -28,7 +31,11 @@ export const ProductCardComponent: React.FC<ProductProps> = (productData) => {
             <div className={styles.addToCartWrapper}>
                 <div className={styles.productCardPrice}>{formattedPrice(productData.price)} ₴</div>
                 <button className={styles.productCardAddToCartButton} type="button" onClick={addToCart}>
-                    <CartIconComponent title="Add to cart" />
+                    {productCartItemCount > 0 ? (
+                        <CartIconCounterComponent counter={productCartItemCount} title="Add to cart" />
+                    ) : (
+                        <CartIconComponent title="Add to cart" />
+                    )}
                 </button>
             </div>
         </div>
