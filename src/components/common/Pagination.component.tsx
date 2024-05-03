36fabafe369bb0926f8174a067cce074e7082e45
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-04 00:07:50 +0300

    add PaginationComponent

---
+++ src/components/common/Pagination.component.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { PaginationNextButtonIconComponent } from '@/components/icon/PaginationNextButtonIcon.component.tsx';
+import { PaginationPreviousButtonIconComponent } from '@/components/icon/PaginationPreviousButtonIcon.component.tsx';
+
+import styles from './pagination.module.css';
+
+interface PaginationProps {
+    page: number;
+    limit: number;
+    total: number;
+    setCurrentPage: (page: number) => void;
+}
+
+export const PaginationComponent: React.FC<PaginationProps> = ({ page, limit, total, setCurrentPage }) => {
+    const totalPages = Math.ceil(total / limit);
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+    const setPreviousPage = (pageNumber: number) => setCurrentPage(pageNumber <= 1 ? 1 : pageNumber - 1);
+    const setNextPage = (pageNumber: number) => setCurrentPage(pageNumber >= totalPages ? totalPages : pageNumber + 1);
+
+    return (
+        <nav>
+            <ul className={styles.paginationList}>
+                <li>
+                    <button
+                        className={`${styles.paginationButton} ${styles.paginationArrowButton}`}
+                        onClick={setPreviousPage.bind(null, page)}
+                        disabled={page === 1}
+                    >
+                        <PaginationPreviousButtonIconComponent className={styles.paginationArrowIcon} title="Previous page" />
+                    </button>
+                </li>
+                {pages.map((pageNumber) => (
+                    <li key={pageNumber}>
+                        <button
+                            className={`${styles.paginationButton} ${pageNumber === page ? styles.paginationButtonActive : ''}`}
+                            onClick={setCurrentPage.bind(null, pageNumber)}
+                        >
+                            {pageNumber}
+                        </button>
+                    </li>
+                ))}
+                <li>
+                    <button
+                        className={`${styles.paginationButton} ${styles.paginationArrowButton}`}
+                        onClick={setNextPage.bind(null, page)}
+                        disabled={page === totalPages}
+                    >
+                        <PaginationNextButtonIconComponent className={styles.paginationArrowIcon} title="Next page" />
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
