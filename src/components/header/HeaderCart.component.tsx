c2c156752209455817dd327e07973ba9b0329fc3
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-03 13:41:24 +0300

    display cart items counter in HeaderCartComponent

--- src/components/header/HeaderCart.component.tsx
+++ src/components/header/HeaderCart.component.tsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
+
+import { CartContext } from '@/context/CartContext.context.tsx';
+
 import { CartIconComponent } from '../icon/CartIcon.component.tsx';
 
 import styles from './header-cart.module.css';
 
-export const HeaderCartComponent = () => (
-    <a href="/cart" className={styles.headerCart}>
-        <CartIconComponent />
-        <div className={styles.headerCartCounter}>0</div>
-    </a>
-);
+export const HeaderCartComponent = () => {
+    const cartContext = useContext(CartContext);
+    const cartItemsCount = cartContext.cart.items.reduce((sum, item) => sum + item.quantity, 0);
+
+    return (
+        <a href="/cart" className={styles.headerCart}>
+            <CartIconComponent />
+            <div className={styles.headerCartCounter}>{cartItemsCount}</div>
+        </a>
+    );
+};
