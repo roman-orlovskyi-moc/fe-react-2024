88d160a465a1fe0aaa16df271f63a7069b1bf29f
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-04 23:28:51 +0300

    move ProductsDataProviderComponent from ProductsListComponent to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AppContextProvider } from '@/context/AppContext.context.tsx';
 import { AboutComponent } from './components/about/About.component.tsx';
 import { FooterComponent } from './components/footer/Footer.component.tsx';
 import { HeaderComponent } from './components/header/Header.component.tsx';
+import { ProductsDataProviderComponent } from './components/products-list/ProductsDataProvider.component.tsx';
 import { ProductsListComponent } from './components/products-list/ProductsList.component.tsx';
 
 import './App.css';
@@ -12,8 +13,10 @@ import './App.css';
 function App() {
     const FULL_NAME: string = `Roman Orlovskyi`;
     const NIK_NAME: string = `roman-orlovskyi-moc`;
+    const PRODUCTS_LIMIT: number = 8;
 
-    const [shouldShowAboutPage, setShouldShowAboutPage] = useState(true);
+    const [shouldShowAboutPage, setShouldShowAboutPage] = useState<boolean>(true);
+    const [currentProductPage, setCurrentProductPage] = useState<number>(1);
 
     const toggleShowAboutPage = (isShowAboutPage: boolean) => {
         setShouldShowAboutPage(isShowAboutPage);
@@ -23,8 +26,21 @@ function App() {
         <AppContextProvider>
             <HeaderComponent shouldShowAboutPage={shouldShowAboutPage} toggleShowAboutPageState={toggleShowAboutPage} />
             <main className="mainContentWrapper contentWrapper">
-                {shouldShowAboutPage && <AboutComponent fullName={FULL_NAME} nikName={NIK_NAME} />}
-                {!shouldShowAboutPage && <ProductsListComponent />}
+                {shouldShowAboutPage ? (
+                    <AboutComponent fullName={FULL_NAME} nikName={NIK_NAME} />
+                ) : (
+                    <ProductsDataProviderComponent page={currentProductPage} limit={PRODUCTS_LIMIT}>
+                        {({ products, productsCount }) => (
+                            <ProductsListComponent
+                                products={products}
+                                productsCount={productsCount}
+                                limit={PRODUCTS_LIMIT}
+                                currentPage={currentProductPage}
+                                setCurrentProductPage={setCurrentProductPage}
+                            />
+                        )}
+                    </ProductsDataProviderComponent>
+                )}
             </main>
             <FooterComponent fullName={FULL_NAME} />
         </AppContextProvider>
--- src/components/products-list/ProductsList.component.tsx
+++ src/components/products-list/ProductsList.component.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react';
+import React from 'react';
 
 import { PaginationComponent } from '@/components/pagination/Pagination.component.tsx';
+import type { ProductProps } from '@/interfaces/ProductProps.interface.tsx';
 
 import { ProductCardComponent } from './ProductCard.component.tsx';
-import { ProductsDataProviderComponent } from './ProductsDataProvider.component.tsx';
 
 import styles from './products-list.module.css';
 
-export const ProductsListComponent = () => {
-    const PRODUCTS_LIMIT = 8;
-    const [currentPage, setCurrentPage] = useState<number>(1);
+interface ProductsListProps {
+    products: ProductProps[];
+    productsCount: number;
+    limit: number;
+    currentPage: number;
+    setCurrentProductPage: (page: number) => void;
+}
 
-    return (
-        <ProductsDataProviderComponent page={currentPage} limit={PRODUCTS_LIMIT}>
-            {(productsData) => (
-                <div>
-                    <ul className={styles.productsList}>
-                        {productsData.products.map((productData) => (
-                            <li className={styles.productListItem} key={productData.id}>
-                                <ProductCardComponent {...productData} />
-                            </li>
-                        ))}
-                    </ul>
-                    <PaginationComponent
-                        page={currentPage}
-                        limit={PRODUCTS_LIMIT}
-                        total={productsData.productsCount}
-                        setCurrentPage={setCurrentPage}
-                    />
-                </div>
-            )}
-        </ProductsDataProviderComponent>
-    );
-};
+export const ProductsListComponent: React.FC<ProductsListProps> = ({
+    products,
+    productsCount,
+    limit,
+    currentPage,
+    setCurrentProductPage,
+}) => (
+    <div>
+        <ul className={styles.productsList}>
+            {products.map((productData) => (
+                <li className={styles.productListItem} key={productData.id}>
+                    <ProductCardComponent {...productData} />
+                </li>
+            ))}
+        </ul>
+        <PaginationComponent page={currentPage} limit={limit} total={productsCount} setCurrentPage={setCurrentProductPage} />
+    </div>
+);
