edf5ef835a37aef068193497ca937f03f7574c14
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-05 11:30:22 +0300

    add ProductsComponent

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,15 @@ import { AppContextProvider } from '@/context/AppContext.context.tsx';
 import { AboutComponent } from './components/about/About.component.tsx';
 import { FooterComponent } from './components/footer/Footer.component.tsx';
 import { HeaderComponent } from './components/header/Header.component.tsx';
-import { ProductsDataProviderComponent } from './components/products-list/ProductsDataProvider.component.tsx';
-import { ProductsListComponent } from './components/products-list/ProductsList.component.tsx';
+import { ProductsComponent } from './components/products/Products.component.tsx';
 
 import './App.css';
 
 function App() {
     const FULL_NAME: string = `Roman Orlovskyi`;
     const NIK_NAME: string = `roman-orlovskyi-moc`;
-    const PRODUCTS_LIMIT: number = 8;
 
     const [shouldShowAboutPage, setShouldShowAboutPage] = useState<boolean>(true);
-    const [currentProductPage, setCurrentProductPage] = useState<number>(1);
 
     const toggleShowAboutPage = (isShowAboutPage: boolean) => {
         setShouldShowAboutPage(isShowAboutPage);
@@ -26,21 +23,7 @@ function App() {
         <AppContextProvider>
             <HeaderComponent shouldShowAboutPage={shouldShowAboutPage} toggleShowAboutPageState={toggleShowAboutPage} />
             <main className="mainContentWrapper contentWrapper">
-                {shouldShowAboutPage ? (
-                    <AboutComponent fullName={FULL_NAME} nikName={NIK_NAME} />
-                ) : (
-                    <ProductsDataProviderComponent page={currentProductPage} limit={PRODUCTS_LIMIT}>
-                        {({ products, productsCount }) => (
-                            <ProductsListComponent
-                                products={products}
-                                productsCount={productsCount}
-                                limit={PRODUCTS_LIMIT}
-                                currentPage={currentProductPage}
-                                setCurrentProductPage={setCurrentProductPage}
-                            />
-                        )}
-                    </ProductsDataProviderComponent>
-                )}
+                {shouldShowAboutPage ? <AboutComponent fullName={FULL_NAME} nikName={NIK_NAME} /> : <ProductsComponent />}
             </main>
             <FooterComponent fullName={FULL_NAME} />
         </AppContextProvider>
--- src/components/products-list/ProductsList.component.tsx
+++ src/components/products-list/ProductsList.component.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { PaginationComponent } from '@/components/pagination/Pagination.component.tsx';
 import type { ProductProps } from '@/interfaces/ProductProps.interface.tsx';
 
 import { ProductCardComponent } from './ProductCard.component.tsx';
@@ -9,27 +8,14 @@ import styles from './products-list.module.css';
 
 interface ProductsListProps {
     products: ProductProps[];
-    productsCount: number;
-    limit: number;
-    currentPage: number;
-    setCurrentProductPage: (page: number) => void;
 }
 
-export const ProductsListComponent: React.FC<ProductsListProps> = ({
-    products,
-    productsCount,
-    limit,
-    currentPage,
-    setCurrentProductPage,
-}) => (
-    <div>
-        <ul className={styles.productsList}>
-            {products.map((productData) => (
-                <li className={styles.productListItem} key={productData.id}>
-                    <ProductCardComponent {...productData} />
-                </li>
-            ))}
-        </ul>
-        <PaginationComponent page={currentPage} limit={limit} total={productsCount} setCurrentPage={setCurrentProductPage} />
-    </div>
+export const ProductsListComponent: React.FC<ProductsListProps> = ({ products }) => (
+    <ul className={styles.productsList}>
+        {products.map((productData) => (
+            <li className={styles.productListItem} key={productData.id}>
+                <ProductCardComponent {...productData} />
+            </li>
+        ))}
+    </ul>
 );
