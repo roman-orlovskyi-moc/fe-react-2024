bda0c5546240d3529b115646cfb39904784148f7
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-06 15:23:27 +0300

    update HeaderComponent

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { AppContext } from '@/context/AppContext.context.tsx';
+import type { AppContextProps } from '@/interfaces/AppContextProps.interface.tsx';
+
+import { CartIconCounterComponent } from '../cart-icon-counter/CartIconCounter.component.tsx';
+import { ColorModeDividerIconComponent } from '../icon/ColorModeDividerIcon.component.tsx';
+import { DarkColorModeIconComponent } from '../icon/DarkColorModeIcon.component.tsx';
+import { LightColorModeIconComponent } from '../icon/LightColorModeIcon.component.tsx';
 import { LogoIconComponent } from '../icon/LogoIcon.component.tsx';
+import { MobileMenuIconComponent } from '../icon/MobileMenuIcon.component.tsx';
 
 import { HeaderAccountComponent } from './HeaderAccount.component.tsx';
-import { HeaderCartComponent } from './HeaderCart.component.tsx';
-import { HeaderMobileMenuComponent } from './HeaderMobileMenu.component.tsx';
-import { HeaderModeSwitcherComponent } from './HeaderModeSwitcher.component.tsx';
-import { HeaderNavigationComponent } from './HeaderNavigation.component.tsx';
 
 import styles from './header.module.css';
 
@@ -15,34 +19,65 @@ interface HeaderProps {
     toggleShowAboutPageState: (isShowAboutPage: boolean) => void;
 }
 
-export const HeaderComponent: React.FC<HeaderProps> = ({ shouldShowAboutPage, toggleShowAboutPageState }) => (
-    <header className={styles.header}>
-        <div className={`contentWrapper ${styles.headerColumnWrapper}`}>
-            <div className={styles.headerLeftColumn}>
-                <div className={styles.headerColumnContainer}>
-                    <a className={styles.headerLogoLink} href="/">
-                        <LogoIconComponent />
-                    </a>
-                    <HeaderModeSwitcherComponent />
+export const HeaderComponent: React.FC<HeaderProps> = ({ shouldShowAboutPage, toggleShowAboutPageState }) => {
+    const appContext: AppContextProps = useContext(AppContext);
+    const cartItemsCount: number = appContext.cart.items.reduce((sum, item) => sum + item.quantity, 0);
+
+    const setDarkColorScheme = () => {
+        appContext.setThemeMode('dark');
+    };
+
+    const setLightColorScheme = () => {
+        appContext.setThemeMode('light');
+    };
+
+    const showAboutPage = () => {
+        toggleShowAboutPageState(true);
+    };
+
+    const showProductsPage = () => {
+        toggleShowAboutPageState(false);
+    };
+
+    return (
+        <header className={styles.header}>
+            <div className={`contentWrapper ${styles.headerColumnWrapper}`}>
+                <a className={styles.headerLogoLink} href="/">
+                    <LogoIconComponent />
+                </a>
+                <div className={styles.headerModeSwitcher}>
+                    <button className={styles.headerModeSwitcherButton} onClick={setLightColorScheme} title="Switch to light mode">
+                        <LightColorModeIconComponent className={appContext.themeMode === 'dark' ? styles.headerModeInactiveIcon : ''} />
+                    </button>
+                    <ColorModeDividerIconComponent />
+                    <button className={styles.headerModeSwitcherButton} onClick={setDarkColorScheme} title="Switch to dark mode">
+                        <DarkColorModeIconComponent className={appContext.themeMode === 'dark' ? '' : styles.headerModeInactiveIcon} />
+                    </button>
                 </div>
+                <ul className={styles.headerNav}>
+                    <li>
+                        <button
+                            className={`${styles.headerNavLink} ${shouldShowAboutPage ? styles.headerNavLinkActive : ''}`}
+                            onClick={showAboutPage}
+                        >
+                            About
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className={`${styles.headerNavLink} ${shouldShowAboutPage ? '' : styles.headerNavLinkActive}`}
+                            onClick={showProductsPage}
+                        >
+                            Products
+                        </button>
+                    </li>
+                </ul>
+                <a href="/cart" className={styles.headerCart}>
+                    <CartIconCounterComponent count={cartItemsCount} wrapperClassName={styles.headerCartCounter} />
+                </a>
+                <HeaderAccountComponent />
+                <MobileMenuIconComponent className={styles.headerMobileMenu} />
             </div>
-            <div className={styles.headerRightColumn}>
-                <div className={styles.headerColumnWrapper}>
-                    <div className={styles.headerLeftColumn}>
-                        <HeaderNavigationComponent
-                            shouldShowAboutPage={shouldShowAboutPage}
-                            toggleShowAboutPageState={toggleShowAboutPageState}
-                        />
-                    </div>
-                    <div className={styles.headerRightColumn}>
-                        <div className={`${styles.headerColumnContainer} ${styles.headerColumnRight}`}>
-                            <HeaderCartComponent />
-                            <HeaderAccountComponent />
-                            <HeaderMobileMenuComponent />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-);
+        </header>
+    );
+};
--- src/components/header/HeaderMobileMenu.component.tsx
+++
@@ -1,9 +0,0 @@
-import { MobileMenuIconComponent } from '../icon/MobileMenuIcon.component.tsx';
-
-import styles from './header-mobile-menu.module.css';
-
-export const HeaderMobileMenuComponent = () => (
-    <div className={styles.headerMobileMenu}>
-        <MobileMenuIconComponent />
-    </div>
-);
