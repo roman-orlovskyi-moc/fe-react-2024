0ab3ea7f8d19a55321e575f5ade825fd0c412769
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-08 00:28:01 +0300

    fix default route path

--- src/context/AppContext.context.tsx
+++ src/context/AppContext.context.tsx
@@ -75,7 +75,7 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({ children
         });
     };
 
-    const [route, setRoute] = useState<string>(window.location.hash);
+    const [route, setRoute] = useState<string>(window.location.hash || '#about');
 
     const setRoutePath = (routePath: string) => {
         setRoute(routePath);
