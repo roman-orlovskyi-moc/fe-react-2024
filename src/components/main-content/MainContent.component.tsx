1c723dfe897838a137003bcddab270c3224165df
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-08 21:35:26 +0300

    use PageNotFoundComponent in MainContentComponent

--- src/components/main-content/MainContent.component.tsx
+++ src/components/main-content/MainContent.component.tsx
@@ -4,6 +4,7 @@ import { AppContext } from '@/context/AppContext.context.tsx';
 import type { AppContextProps } from '@/interfaces/AppContextProps.interface.tsx';
 
 import { AboutComponent } from '../about/About.component.tsx';
+import { PageNotFoundComponent } from '../page-not-found/PageNotFound.component.tsx';
 import { ProductsComponent } from '../products/Products.component.tsx';
 
 interface MainContentProps {
@@ -14,5 +15,11 @@ interface MainContentProps {
 export const MainContentComponent: React.FC<MainContentProps> = ({ fullName, nikName }) => {
     const appContext: AppContextProps = useContext(AppContext);
 
-    return appContext.route === '#products' ? <ProductsComponent /> : <AboutComponent fullName={fullName} nikName={nikName} />;
+    if (appContext.route === '#products') {
+        return <ProductsComponent />;
+    } else if (appContext.route === '#about') {
+        return <AboutComponent fullName={fullName} nikName={nikName} />;
+    } else {
+        return <PageNotFoundComponent />;
+    }
 };
