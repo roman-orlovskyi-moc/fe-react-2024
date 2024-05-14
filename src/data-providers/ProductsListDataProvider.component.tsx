03954343388f6ef3855b572333dac3cb866b86e2
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-14 23:19:50 +0300

    add ability to filter and sort products to ProductsListDataProviderComponent

--- src/data-providers/ProductsListDataProvider.component.tsx
+++ src/data-providers/ProductsListDataProvider.component.tsx
@@ -9,19 +9,60 @@ interface ProductsDataProps {
     productsCount: number;
 }
 
+interface ProductsDataProviderFilterProps {
+    title?: string;
+    categoryId?: number;
+}
+
+type SortDirection = 'asc' | 'desc';
+
+interface ProductsDataProviderSortProps {
+    price?: SortDirection;
+    creationAt?: SortDirection;
+}
+
 interface ProductsDataProviderProps {
     page: number;
     limit: number;
+    filter?: ProductsDataProviderFilterProps;
+    sort?: ProductsDataProviderSortProps;
     children: (productsData: ProductsDataProps) => React.ReactNode;
 }
 
-export const ProductsListDataProviderComponent: React.FC<ProductsDataProviderProps> = ({ page, limit, children }) => {
+export const ProductsListDataProviderComponent: React.FC<ProductsDataProviderProps> = ({ page, limit, filter, sort, children }) => {
+    let filteredProducts = productsJSONData;
+
+    if (filter?.title) {
+        const filterTitle = filter.title.toLowerCase();
+
+        filteredProducts = filteredProducts.filter((product) => product.title.toLowerCase().includes(filterTitle));
+    }
+
+    if (filter?.categoryId) {
+        filteredProducts = filteredProducts.filter((product) => product.category.id === filter.categoryId);
+    }
+
+    if (sort?.price) {
+        filteredProducts = filteredProducts.sort((firstProduct, secondProduct) =>
+            sort.price === 'asc' ? firstProduct.price - secondProduct.price : secondProduct.price - firstProduct.price,
+        );
+    }
+
+    if (sort?.creationAt) {
+        filteredProducts = filteredProducts.sort((firstProduct, secondProduct) => {
+            const firstDate = new Date(firstProduct.creationAt).getTime();
+            const secondDate = new Date(secondProduct.creationAt).getTime();
+
+            return sort.creationAt === 'asc' ? firstDate - secondDate : secondDate - firstDate;
+        });
+    }
+
     const start = (page - 1) * limit;
     const end = start + limit;
 
     const productsData = {
-        productsCount: productsJSONData.length,
-        products: productsJSONData.slice(start, end) as ProductProps[],
+        productsCount: filteredProducts.length,
+        products: filteredProducts.slice(start, end) as ProductProps[],
     };
 
     return children(productsData);
