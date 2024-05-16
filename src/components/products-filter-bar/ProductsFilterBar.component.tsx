dc6ccbf8cba786bd373ebe50a0aea868698a3614
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-05-16 23:47:34 +0300

    update ProductsFilterBarComponent styles

--- src/components/products-filter-bar/ProductsFilterBar.component.tsx
+++ src/components/products-filter-bar/ProductsFilterBar.component.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 
+import appStyles from '@/App.module.css';
+import { SearchIconComponent } from '@/components/icon/SearchIcon.component.tsx';
 import { CategoriesDataProviderComponent } from '@/data-providers/CategoriesDataProvider.component.tsx';
 import { transformCheckedCategories, transformCheckedCategoryIds } from '@/helpers/productsFilterBarHelper.ts';
 import type { CheckedCategoriesProps } from '@/types/CheckedCategoriesProps.type.tsx';
+
+import { DropdownComponent } from '../dropdown/Dropdown.component.tsx';
+
+import styles from './products-filter-bar.module.css';
 
 interface ProductsFilterBarProps {
     search?: string;
@@ -44,47 +50,66 @@ export const ProductsFilterBarComponent: React.FC<ProductsFilterBarProps> = ({
         setProductsCategories(transformCheckedCategoryIds(updatedCheckedCategories));
     };
 
-    const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const target = event.target as HTMLSelectElement;
-
-        setSortValue(target.value);
-        setProductsSort(target.value);
+    const handleSortChange = (value: string) => {
+        setSortValue(value);
+        setProductsSort(value);
     };
 
     return (
-        <div>
-            <form onSubmit={handleSearchSubmit}>
-                <input type="text" placeholder="Search" value={searchTitle} onChange={handleSearchChange} />
-                <button type="submit">Search</button>
+        <div className={styles.productsFilterBar}>
+            <form className={styles.productsFilterSearchForm} onSubmit={handleSearchSubmit}>
+                <input
+                    className={styles.productsFilterSearchInput}
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTitle}
+                    onChange={handleSearchChange}
+                />
+                <button className={styles.productsFilterSearchButton} type="submit">
+                    <SearchIconComponent title="Search" />
+                </button>
             </form>
-            <div>
+            <div className={styles.productsFilterCategoryWrapper}>
                 <CategoriesDataProviderComponent>
                     {({ categories }) => (
                         <>
                             {categories.map((categoryData) => (
-                                <label key={categoryData.id}>
+                                <>
                                     <input
                                         type="checkbox"
+                                        id={`category_checkbox_${categoryData.id}`}
                                         name={`category_${categoryData.id}`}
                                         value={categoryData.id}
                                         checked={checkedCategories[`${categoryData.id}`] || false}
                                         onChange={handleCategoryCheck}
+                                        className={styles.productsFilterCategoryCheckbox}
+                                        aria-hidden={true}
                                     />
-                                    {categoryData.name}
-                                </label>
+                                    <label
+                                        key={categoryData.id}
+                                        htmlFor={`category_checkbox_${categoryData.id}`}
+                                        className={`${appStyles.button} ${styles.productsFilterCategoryButton}`}
+                                    >
+                                        {categoryData.name}
+                                    </label>
+                                </>
                             ))}
                         </>
                     )}
                 </CategoriesDataProviderComponent>
             </div>
-            <div>
+            <div className={styles.productsFilterSortWrapper}>
                 Sort by:
-                <select value={sortValue} onChange={handleSortChange}>
-                    <option value="price:desc">Price: High to Low</option>
-                    <option value="price:asc">Price: Low to High</option>
-                    <option value="creationAt:asc">Newest</option>
-                    <option value="creationAt:desc">Oldest</option>
-                </select>
+                <DropdownComponent
+                    options={[
+                        { value: 'price:desc', label: 'Price: High to Low' },
+                        { value: 'price:asc', label: 'Price: Low to High' },
+                        { value: 'creationAt:asc', label: 'Newest' },
+                        { value: 'creationAt:desc', label: 'Oldest' },
+                    ]}
+                    onDropdownChange={handleSortChange}
+                    selectedValue={sortValue}
+                />
             </div>
         </div>
     );
