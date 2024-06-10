badfc31f399db91c060d045fb4f47b47cb4ddd9c
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-06-10 22:55:55 +0300

    handle cart item interaction in CartItemComponent

--- src/components/cart-item/CartItem.component.tsx
+++ src/components/cart-item/CartItem.component.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { ROUTES } from '@/constants/Routes.constant.ts';
 import { formatPrice } from '@/helpers/ProductDetails.helper.tsx';
+import { useCart } from '@/hooks/UseCart.hook.ts';
 import type { CartItemProduct } from '@/interfaces/CartItemProduct.interface.ts';
 
 import { MinusIconComponent } from '../icon/MinusIcon.component.tsx';
@@ -16,6 +17,35 @@ interface CartItemProps {
 }
 
 export const CartItemComponent: React.FC<CartItemProps> = ({ cartItemProduct }) => {
+    const [quantity, setQuantity] = useState<number>(cartItemProduct.quantity);
+    const { updateCartItemQuantity, removeCartItem } = useCart();
+
+    useEffect(() => {
+        if (quantity !== cartItemProduct.quantity) {
+            updateCartItemQuantity(cartItemProduct.id, quantity);
+        }
+    }, [quantity, updateCartItemQuantity, cartItemProduct]);
+
+    const handleDecreaseQuantity = () => {
+        setQuantity((previousQuantity) => (previousQuantity > 1 ? previousQuantity - 1 : 1));
+    };
+
+    const handleIncreaseQuantity = () => {
+        setQuantity((previousQuantity) => previousQuantity + 1);
+    };
+
+    const handleInputChangeQuantity = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newQuantity = Number.parseInt(event.target.value, 10);
+
+        if (!Number.isNaN(newQuantity) && newQuantity >= 1) {
+            setQuantity(newQuantity);
+        }
+    };
+
+    const handleRemoveItem = () => {
+        removeCartItem(cartItemProduct.id);
+    };
+
     const cartItemProductLink = `${ROUTES.PRODUCTS}/${cartItemProduct.id}`;
 
     return (
@@ -33,11 +63,11 @@ export const CartItemComponent: React.FC<CartItemProps> = ({ cartItemProduct }) 
                 </div>
             </div>
             <div className={styles.cartItemQuantityContainer}>
-                <button className={styles.cartItemQuantityButton}>
+                <button className={styles.cartItemQuantityButton} onClick={handleDecreaseQuantity}>
                     <MinusIconComponent className={styles.cartItemQuantityButtonIcon} title="Decrease quantity" />
                 </button>
-                <input className={styles.cartItemQuantityInput} defaultValue={cartItemProduct.quantity} />
-                <button className={styles.cartItemQuantityButton}>
+                <input className={styles.cartItemQuantityInput} value={quantity} onChange={handleInputChangeQuantity} />
+                <button className={styles.cartItemQuantityButton} onClick={handleIncreaseQuantity}>
                     <PlusIconComponent className={styles.cartItemQuantityButtonIcon} title="Increase quantity" />
                 </button>
             </div>
@@ -46,7 +76,7 @@ export const CartItemComponent: React.FC<CartItemProps> = ({ cartItemProduct }) 
                 <span className={styles.cartItemTotalPriceUnit}>₴</span>
             </div>
             <div className={styles.cartItemRemoveContainer}>
-                <button className={styles.cartItemRemoveButton}>
+                <button className={styles.cartItemRemoveButton} onClick={handleRemoveItem}>
                     <TrashIconComponent className={styles.cartItemRemoveIcon} title="Remove item" />
                 </button>
             </div>
